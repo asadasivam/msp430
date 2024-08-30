abcf0367f776a7687ccaeba13a73a11ca204050b
asadasivam <askshiva89@gmail.com>
2024-08-30 22:47:37 +0530

    cpp wrappers added

--- 001_hw_logic/Debug/main.d
+++ 001_hw_logic/Debug/main.d
@@ -1,13 +1,13 @@
 # FIXED
 
-main.obj: ../main.c
+main.obj: ../main.cpp
 main.obj: /home/sadash/ti/ccs1280/ccs/ccs_base/msp430/include/msp430.h
 main.obj: /home/sadash/ti/ccs1280/ccs/ccs_base/msp430/include/msp430fr5969.h
 main.obj: /home/sadash/ti/ccs1280/ccs/ccs_base/msp430/include/in430.h
 main.obj: /home/sadash/ti/ccs1280/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
 main.obj: /home/sadash/ti/ccs1280/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
 
-../main.c:
+../main.cpp:
 
 /home/sadash/ti/ccs1280/ccs/ccs_base/msp430/include/msp430.h:
 
---
+++ 001_hw_logic/gIOClass.cpp
@@ -0,0 +1,41 @@
+/*
+ * IODriver.cpp
+ *
+ *  Created on: 30-Aug-2024
+ *      Author: sadash
+ */
+
+#include <gIOClass.h>
+
+gIOClass::gIOClass()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+gIOClass::~gIOClass()
+{
+    // TODO Auto-generated destructor stub
+}
+
+bool gIOClass::SchmiddTrgger_Configure()
+{
+    //reading the base address of GPIO registers
+
+    return 0;
+}
+bool gIOClass::Comparator_Configure()
+{
+    //reading the base address of GPIO registers
+
+    return 0;
+}
+/*
+ * port configuration
+ */
+gpioErrors gIOClass::gpio_Configure()
+{
+    //reading the base address of GPIO registers
+
+    return ;
+}
