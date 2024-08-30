9e521787be9da25f72ac9cb6996628ecc8c6decc
asadasivam <askshiva89@gmail.com>
2024-08-30 22:50:22 +0530

    cs and prescalar added

---
+++ 001_hw_logic/gPreScaler.cpp
@@ -0,0 +1,20 @@
+/*
+ * gPreScaler.cpp
+ *
+ *  Created on: 30-Aug-2024
+ *      Author: sadash
+ */
+
+#include <gPreScaler.h>
+
+gPreScaler::gPreScaler()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+gPreScaler::~gPreScaler()
+{
+    // TODO Auto-generated destructor stub
+}
+
